7dbe4a635420f397c1390f1d6162ecdd212f8db1
Altonhe <al648228665@gmail.com>
2024-08-09 03:26:50 -0400

    fix lint

--- modules/digitalocean-droplets/variables.tf
+++ modules/digitalocean-droplets/variables.tf
@@ -6,20 +6,25 @@ variable "do_token" {
 
 variable "vm_name" {
   description = "Name of the Droplet"
+  type        = string
 }
 
 variable "region" {
   description = "DigitalOcean region"
+  type        = string
 }
 
 variable "size" {
   description = "Droplet size"
+  type        = string
 }
 
 variable "user" {
   description = "user name"
+  type        = string
 }
 
 variable "caddy" {
   description = "Whether to use Caddy or not, leave empty to disable"
+  type        = string
 }
